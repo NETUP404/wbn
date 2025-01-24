7db66db7c20f7688553a554c36c7ab77ddcbd22f
NETUP404 <NETUPSOLUTIONS@PROTON.ME>
2025-01-24 05:22:12 +0100

    Update class-wbn-reports.php

--- includes/class-wbn-reports.php
+++ includes/class-wbn-reports.php
@@ -1,17 +1,15 @@
-class-wbn-report<?php
+<?php
 
 class WBN_Reports {
     public function __construct() {
-        // Inicializar funciones de reportes
         add_shortcode('wbn_user_dashboard', array($this, 'render_user_dashboard'));
     }
 
     public function render_user_dashboard() {
-        // Renderizar el panel de usuario
         ob_start();
         echo '<div class="wbn-dashboard"><h2>Panel de Usuario</h2></div>';
         return ob_get_clean();
     }
 }
 
-new WBN_Reports();s.php
+new WBN_Reports();
