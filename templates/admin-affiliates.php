798633dbefa1b4516db681403bdd2cfceecf8b14
NETUP404 <NETUPSOLUTIONS@PROTON.ME>
2025-01-24 05:58:30 +0100

    Add files via upload

---
+++ templates/admin-affiliates.php
@@ -0,0 +1,5 @@
+<?php
+
+// Aquí puedes agregar el contenido dinámico del panel de administrador para los afiliados
+
+?>
---
+++ templates/admin-campaigns.php
@@ -0,0 +1,5 @@
+<?php
+
+// Aquí puedes agregar el contenido dinámico del panel de administrador para las campañas
+
+?>
