c9917d3324db81228ba673a1cf6ddcfab952b564
NETUP404 <NETUPSOLUTIONS@PROTON.ME>
2025-01-24 05:27:11 +0100

    Create admin-dashboard.php

---
+++ templates/admin-dashboard.php
@@ -0,0 +1,16 @@
+<?php
+/* Template Name: Panel Admin */
+
+get_header();
+?>
+
+<div class="wrap">
+    <h1>Panel Admin</h1>
+    <div class="wbn-dashboard">
+        <?php
+        // Aquí puedes agregar el contenido dinámico del panel de administrador
+        ?>
+    </div>
+</div>
+
+<?php get_footer(); ?>
